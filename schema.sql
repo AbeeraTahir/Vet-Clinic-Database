ca3c68d2e83c5d41771c3e11d6322e6f8e2eeadf
Abeera Tahir <abeeratahir2018@gmail.com>
2023-01-18 19:55:59 +0500

    Create and modify tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species VARCHAR(100);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INT
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
